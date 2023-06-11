8b270ce5dee2124048494e2eb4d60afdd0d34a56
amir <amirrroff@gmail.com>
2023-06-11 18:04:41 +0200

    change some routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import { useState } from "react";
-import Game from "./Game";
+import { useState } from 'react';
+import Game from './Game';
 
-  function App() {
-    const [play , setPlay] = useState(false)
+function App() {
+  const [play, setPlay] = useState(false);
 
-    const handlePalay = () =>{
-      setPlay(true)
-    }
-    return (
-    <div className="">
-            <button className={play ? "hidden":' border-cyan-500 border-2 bg-cyan-500 rounded-lg px-12 py-2 shadow-2xl hover:bg-cyan-600 text-white text-3xl font-bold mt-96'} onClick={handlePalay}>play</button>
-      {play && <Game />}
-    
+  const handlePlay = () => {
+    setPlay(true);
+  };
+  const handleBack = () => {
+    setPlay(false);
+  };
+  return (
+    <div className="flex items-center justify-center h-screen flex-col">
+      <div className="mb-4">
+        {!play && (
+          <button
+            className=" border-cyan-500 border-2 bg-cyan-500 rounded-lg px-12 py-2 shadow-2xl hover:bg-cyan-600 text-white text-3xl font-bold w-96"
+            onClick={handlePlay}
+          >
+            Fruit Bingo
+          </button>
+        )}
+        {play && <Game onBack={handleBack} />}
+      </div>
+      {/*
+       <div>
+       {!play && (
+        <button  className=' border-cyan-500 border-2 bg-cyan-500 rounded-lg px-12 py-2 shadow-2xl hover:bg-cyan-600 text-white text-3xl font-bold w-96' onClick={handlePlay}>Sudoko</button>
+      )}
+      
+        </div>
+       */}
     </div>
   );
 }
--- src/Game.js
+++ src/Game.js
@@ -1,94 +1,96 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import SingleCart from './SingleCart/SingleCart';
+import { IoArrowBack } from 'react-icons/io5';
 
+const Game = ({ onBack }) => {
+  const cardImages = [
+    { src: '/assets/Cucumber.png', matched: false },
+    { src: '/assets/Lemon.png', matched: false },
+    { src: '/assets/Melon.png', matched: false },
+    { src: '/assets/Pear.png', matched: false },
+    { src: '/assets/Watermelon.png', matched: false },
+    { src: '/assets/Cantaloupe.png', matched: false },
+  ];
 
-const Game = () => {
-    const cardImages =[
-        { "src": "/assets/Cucumber.png",matched:false },
-        { "src": "/assets/Lemon.png" ,matched:false },
-        { "src": "/assets/Melon.png",matched:false  },
-        { "src": "/assets/Pear.png" ,matched:false },
-        { "src": "/assets/Watermelon.png" ,matched:false },
-        { "src": "/assets/Cantaloupe.png",matched:false  },
-      ]
-      
-      
-        const [cards, setCards] = useState([])
-        const [turns, setTurns] = useState(0)
-        const [choiseOne, setChoiceOne] =useState(null)
-        const [choiseTwo, setChoiseTwo] = useState(null)
-        const [disabled , setDisabled] = useState(false)
-      
-      
-        const handleCarts = () =>{
-          const handleCarts =[...cardImages, ...cardImages]
-          .sort(()=>Math.random() - 0.5)
-          .map((card)=> ({...card, id: Math.random()}))
-      
-          setCards(handleCarts)
-          setTurns(0)
-          setChoiceOne(null)
-          setChoiseTwo(null)
-        }
-      
-      
-        const handleChoise = (card) =>{
-          choiseOne ? setChoiseTwo(card) : setChoiceOne(card)
-        }
-      
-        useEffect(()=>{
-          
-          if(choiseOne && choiseTwo) {
-            setDisabled(true)
-            if( choiseOne.src === choiseTwo.src){
-              setCards(prevCards =>{
-                  return prevCards.map(card => {
-                    if (card.src === choiseOne.src) {
-                      return {...card, matched: true}
-                    } else {
-                       return card
-                    }
-                  })
-              })
-              resetTurn()
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiseOne, setChoiceOne] = useState(null);
+  const [choiseTwo, setChoiseTwo] = useState(null);
+  const [disabled, setDisabled] = useState(false);
+
+  const handleCarts = () => {
+    const handleCarts = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(handleCarts);
+    setTurns(0);
+    setChoiceOne(null);
+    setChoiseTwo(null);
+  };
+
+  const handleChoise = (card) => {
+    choiseOne ? setChoiseTwo(card) : setChoiceOne(card);
+  };
+
+  useEffect(() => {
+    if (choiseOne && choiseTwo) {
+      setDisabled(true);
+      if (choiseOne.src === choiseTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiseOne.src) {
+              return { ...card, matched: true };
             } else {
-                setTimeout(()=> resetTurn(), 1000)
+              return card;
             }
-          }
-        }, [choiseOne, choiseTwo])
-      
-        console.log(cards)
-      
-        const resetTurn = () =>{
-          setDisabled(false)
-          setChoiceOne(null)
-          setChoiseTwo(null)
-          setTurns(prevTurns => prevTurns + 1)
-        }
-      
-          useEffect(()=>{
-            handleCarts()
-          },[])
-    return ( 
-        <div>
-             <h1 className='text-rose-700 text-4xl font-bold mb-6'>Amirrroff Game</h1>
-      <button className=' border-cyan-500 border-2 bg-cyan-500 rounded-lg px-12 py-2 shadow-2xl hover:bg-cyan-600 text-white text-3xl font-bold' onClick={handleCarts}>Reset</button>
-      
-      <div className='grid mt-10 gap-5 grid-cols-6  p-2'>
-        {cards.map(card => (
-         <SingleCart
-          card={card}
-          key={card.id}
-          handleChoise={handleChoise}
-          flipped = {card === choiseOne || card === choiseTwo || card.matched}
-          disabled={disabled}
-           />
+          });
+        });
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiseOne, choiseTwo]);
+
+  console.log(cards);
+
+  const resetTurn = () => {
+    setDisabled(false);
+    setChoiceOne(null);
+    setChoiseTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  useEffect(() => {
+    handleCarts();
+  }, []);
+  return (
+    <div>
+      <IoArrowBack onClick={onBack} className="hover:text-slate-600 text-4xl" />
+      <h1 className="text-rose-700 text-4xl font-bold mb-6">Amirrroff Game</h1>
+      <button
+        className=" border-cyan-500 border-2 bg-cyan-500 rounded-lg px-12 py-2 shadow-2xl hover:bg-cyan-600 text-white text-3xl font-bold"
+        onClick={handleCarts}
+      >
+        Reset
+      </button>
+
+      <div className="grid mt-10 gap-5 grid-cols-6  p-2">
+        {cards.map((card) => (
+          <SingleCart
+            card={card}
+            key={card.id}
+            handleChoise={handleChoise}
+            flipped={card === choiseOne || card === choiseTwo || card.matched}
+            disabled={disabled}
+          />
         ))}
       </div>
-      <p className='border bg-rose-600 font-bold'> Turns: {turns}</p>
-        </div>
-     );
-}
- 
+      <p className="border bg-rose-600 font-bold"> Turns: {turns}</p>
+    </div>
+  );
+};
+
 export default Game;
